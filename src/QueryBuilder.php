86a01440e2cdd8b046a26422fcbf32cfa0ae69f3
Luis Nascimento <luis.nascimento@taotesting.com>
2020-05-29 16:46:06 +0200

    First implementation of the query builder

--- src/QueryBuilder.php
+++ src/QueryBuilder.php
@@ -23,6 +23,23 @@ namespace oat\tao\elasticsearch;
 
 class QueryBuilder
 {
+    private const STANDARD_FIELDS = [
+        'class',
+        'content',
+        'label',
+        'model',
+        'login',
+    ];
+
+    private const CUSTOM_FIELDS = [
+        'HTMLArea',
+        'TextArea',
+        'TextBox',
+        'ComboBox',
+        'Checkbox',
+        'RadioBox',
+    ];
+
     public static function create(): self
     {
         return new self();
@@ -30,36 +47,41 @@ class QueryBuilder
 
     public function getSearchParams(string $queryString, string $type, int $start, int $count, string $order, string $dir): array
     {
-        $parts = explode(' ', htmlspecialchars_decode($queryString));
+        $qs = htmlspecialchars_decode($queryString);
+        $blocks = explode('AND', $qs);
+        $query = [];
 
-        foreach ($parts as $key => $part) {
-            $matches = [];
-            $part = $this->updateIfUri($part);
-            if (preg_match('/^([^a-z_]*)([a-z_]+):(.*)/', $part, $matches) === 1) {
-                [$fullstring, $prefix, $fieldname, $value] = $matches;
-                $value = $this->updateIfUri($value);
-                if ($fieldname) {
-                    $parts[$key] = $prefix . $fieldname . ':' . str_replace(':', '\\:', $value);
-                }
+        foreach ($blocks as $block) {
+            preg_match('/((?P<field>.*):)?(?P<term>.*)/', $block,$matches);
+            $field = strtolower(trim($matches['field']));
+
+            $term = trim($matches['term']);
+            $term = $this->updateIfUri($term);
+
+            if (empty($field)) {
+                $query[] = sprintf('(%s)', $term);
+            } elseif ($this->isStandardField($field)) {
+                $query[] = sprintf('(%s:%s)', $field, $term);
+            } else {
+                $query[] = $this->buildCustomConditions($field, $term);
             }
         }
-        $queryString = implode(' ', $parts);
-        $queryString = (strlen($queryString) == 0 ? '' : '(' . $queryString . ') AND ')
-            . 'type:' . str_replace(':', '\\:', '"' . $type . '"');
+
+        $query[] = 'type:' . str_replace(':', '\\:', '"' . $type . '"');
 
         $query = [
             'query' => [
                 'query_string' =>
                     [
                         "default_operator" => "AND",
-                        "query" => $queryString
+                        "query" => $queryString = implode(' AND ', $query)
                     ]
             ],
             'sort' => [$order => ['order' => $dir]]
         ];
 
         $params = [
-            "index" => implode(',', IndexerInterface::AVAILABLE_INDEXES), //TODO we need to specificy only one index during implementation of task (TAO-10248)
+            "index" => $this->getIndexByType($type),
             "size" => $count,
             "from" => $start,
             "client" => ["ignore" => 404],
@@ -69,11 +91,32 @@ class QueryBuilder
         return $params;
     }
 
-    protected function updateIfUri(string $query): string
+    private function isStandardField(string $field): bool
+    {
+        return in_array(strtolower($field), self::STANDARD_FIELDS);
+    }
+
+    private function getIndexByType(string $type): string
+    {
+        return IndexerInterface::AVAILABLE_INDEXES[$type];
+    }
+
+    private function updateIfUri(string $query): string
     {
         if (\common_Utils::isUri($query)) {
             $query = '"' . $query . '"';
         }
         return $query;
+    }
+
+    private function buildCustomConditions(string $fieldName, string $term): string
+    {
+        $conditions = [];
+
+        foreach (self::CUSTOM_FIELDS as $customField) {
+            $conditions[] = sprintf('%s_%s:%s', $customField, $fieldName, $term);
+        }
+
+        return '(' . implode(' OR ', $conditions). ')';
     }
 }
