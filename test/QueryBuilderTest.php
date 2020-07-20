524c5147ebc6e6fa920bfc7b2ba0d8355dede614
Luis Nascimento <luis.nascimento@taotesting.com>
2020-07-20 15:42:51 +0200

    TAO-10365 Updated changelog and added test cases for AND condition written with Capital/Lower/Mixed letters

--- test/QueryBuilderTest.php
+++ test/QueryBuilderTest.php
@@ -72,6 +72,14 @@ class QueryBuilderTest extends TestCase
             [
                 'label:test AND custom_field:test',
                 '{"query":{"query_string":{"default_operator":"AND","query":"(label:\"test\") AND (HTMLArea_custom-field:\"test\" OR TextArea_custom-field:\"test\" OR TextBox_custom-field:\"test\" OR ComboBox_custom-field:\"test\" OR CheckBox_custom-field:\"test\" OR RadioBox_custom-field:\"test\")"}},"sort":{"_id":{"order":"DESC"}}}',
+            ],
+            [
+                'label:test and custom_field:test',
+                '{"query":{"query_string":{"default_operator":"AND","query":"(label:\"test\") AND (HTMLArea_custom-field:\"test\" OR TextArea_custom-field:\"test\" OR TextBox_custom-field:\"test\" OR ComboBox_custom-field:\"test\" OR CheckBox_custom-field:\"test\" OR RadioBox_custom-field:\"test\")"}},"sort":{"_id":{"order":"DESC"}}}',
+            ],
+            [
+                'label:test aNd custom_field:test',
+                '{"query":{"query_string":{"default_operator":"AND","query":"(label:\"test\") AND (HTMLArea_custom-field:\"test\" OR TextArea_custom-field:\"test\" OR TextBox_custom-field:\"test\" OR ComboBox_custom-field:\"test\" OR CheckBox_custom-field:\"test\" OR RadioBox_custom-field:\"test\")"}},"sort":{"_id":{"order":"DESC"}}}',
             ]
         ];
     }
