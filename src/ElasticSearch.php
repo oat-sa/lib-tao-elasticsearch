eba9454a1fd886aaea3b8b1f02f3a2367541c4c3
bartlomiej <bartlomiej@taotesting.com>
2020-12-01 16:02:05 +0100

    Remove uri key

--- src/ElasticSearch.php
+++ src/ElasticSearch.php
@@ -183,7 +183,7 @@ class ElasticSearch extends ConfigurableService implements Search
         $total = 0;
         if ($elasticResult && isset($elasticResult['hits'])) {
             foreach ($elasticResult['hits']['hits'] as $document) {
-                $uris[$document['_id']] = $document['_source'];
+                $uris[] = $document['_source'];
             }
             // Starts from Elasticsearch 7.0 the `total` attribute is object with two parameters [value,relation]
             $total = is_array($elasticResult['hits']['total'])
